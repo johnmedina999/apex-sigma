72fdd3632efbb37aaa95bc99b963f2819452610c
abraker95 <abraker95@gmail.com>
2017-05-07 19:47:14 -0400

    Code cleanup; Display server name in title; attempt to fix listing

--- sigma/plugins/points/ranking.py
+++ sigma/plugins/points/ranking.py
@@ -2,33 +2,30 @@ import discord
 
 async def ranking(cmd, message, args):
 
-    user_id = message.author
     query = {
-        #'UserID': user_id,
         'ServerID': message.server.id
     }
 
-    embed = discord.Embed(title=':gem: Sigma Ranking Top 10', color=0x0099FF)
+    embed = discord.Embed(title=':gem: ' + message.server.name + ' Ranking Top 10', color=0x0099FF)
     number_grabber = cmd.db.find('PointSystem', query).sort('Points', -1)
 
     points = ''
     users = ''
     numbers = ''
-
     count = 0
+
     for result in number_grabber:
         try:
-            points = points + str(result['Points']) + "\n"
+            if count > 10: break
+            points += str(result['Points']) + "\n"
             users += message.server.get_member(result['UserID']).name + "\n"
 
             count += 1
-            if count > 10: break
             numbers += str(count) + "\n"
-        except:
-            pass
+        except: pass
 
     embed.add_field(name='#', value=numbers)
-    embed.add_field(name='Users', value=str(users))
+    embed.add_field(name='Users', value=users)
     embed.add_field(name='Points', value=points)
 
     await cmd.bot.send_message(message.channel, None, embed=embed)
