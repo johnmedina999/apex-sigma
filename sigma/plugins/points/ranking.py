814c625bc32a05ac2eb43356e3a020084a52136f
abraker95 <abraker95@gmail.com>
2017-05-07 19:33:22 -0400

    A ranking command that actually displays the rankings

--- sigma/plugins/points/ranking.py
+++ sigma/plugins/points/ranking.py
@@ -1,9 +1,34 @@
 import discord
-from config import MainServerURL
-
 
 async def ranking(cmd, message, args):
-    embed = discord.Embed(color=0x1ABC9C)
-    embed.add_field(name='Sigma Ranking for ' + message.server.name,
-                    value='You can click [HERE](' + MainServerURL + 'ranking?sid=' + message.server.id + ') to see the Sigma Ranking for this server.')
+
+    user_id = message.author
+    query = {
+        #'UserID': user_id,
+        'ServerID': message.server.id
+    }
+
+    embed = discord.Embed(title=':gem: Sigma Ranking Top 10', color=0x0099FF)
+    number_grabber = cmd.db.find('PointSystem', query).sort('Points', -1)
+
+    points = ''
+    users = ''
+    numbers = ''
+
+    count = 0
+    for result in number_grabber:
+        try:
+            points = points + str(result['Points']) + "\n"
+            users += message.server.get_member(result['UserID']).name + "\n"
+
+            count += 1
+            if count > 10: break
+            numbers += str(count) + "\n"
+        except:
+            pass
+
+    embed.add_field(name='#', value=numbers)
+    embed.add_field(name='Users', value=str(users))
+    embed.add_field(name='Points', value=points)
+
     await cmd.bot.send_message(message.channel, None, embed=embed)
