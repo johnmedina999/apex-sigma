3f487d69656ac53c24be47726dae3e25723fb0e0
abraker95 <abraker95@gmail.com>
2017-05-07 20:14:03 -0400

    Attempt to fix ranking listing #3 and some refactoring

--- sigma/plugins/points/ranking.py
+++ sigma/plugins/points/ranking.py
@@ -9,23 +9,26 @@ async def ranking(cmd, message, args):
     embed = discord.Embed(title=':gem: ' + message.server.name + ' Ranking Top 10', color=0x0099FF)
     number_grabber = cmd.db.find('PointSystem', query).sort('Points', -1)
 
-    points = ''
-    users = ''
-    numbers = ''
+    rankingData = {'numbers': '', 'users': '', 'points': ''}
     count = 0
 
     for result in number_grabber:
+        number = user = points = ''
         try:
             count += 1
             if count > 10: break
 
-            numbers += str(count) + "\n"
-            users += message.server.get_member(result['UserID']).name + "\n"
-            points += str(result['Points']) + "\n"
+            number = str(count) + "\n"
+            user = message.server.get_member(result['UserID']).name + "\n"
+            points = str(result['Points']) + "\n"
         except: pass
 
-    embed.add_field(name='#', value=numbers)
-    embed.add_field(name='Users', value=users)
-    embed.add_field(name='Points', value=points)
+        rankingData['numbers'] += number
+        rankingData['users'] += user
+        rankingData['points'] += points
+
+    embed.add_field(name='#', value=rankingData['numbers'])
+    embed.add_field(name='Users', value=rankingData['users'])
+    embed.add_field(name='Points', value=rankingData['points'])
 
     await cmd.bot.send_message(message.channel, None, embed=embed)
