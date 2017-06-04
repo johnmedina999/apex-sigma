736b35be93d7a354482417ce248a2e8c9e6254ed
abraker95 <abraker95@gmail.com>
2017-06-04 13:23:54 -0400

    Fix pun text being cut off

--- sigma/plugins/fun/pun.py
+++ sigma/plugins/fun/pun.py
@@ -5,12 +5,16 @@ import discord
 async def pun(cmd, message, args):
     cmd.db.add_stats('CancerCount')
     pun_url = 'http://www.punoftheday.com/cgi-bin/arandompun.pl'
+
     async with aiohttp.ClientSession() as session:
         async with session.get(pun_url) as data:
             pun_req = await data.text()
-    pun_text = (str(pun_req)[len('b\'document.write(\\\'&quot;'):-len(
-        '&quot;<br />\\\')\ndocument.write(\\\'<i>&copy; 1996-2016 <a href="http://www.punoftheday.com">Pun of the Day.com</a></i><br />\\\')\\n\'') - 1]).replace(
-        '&rsquo;', '\'')
+
+    pun_text = str(pun_req)
+    pun_text = pun_text[pun_text.find('&quot;') + len('&quot;') : len(pun_text)]  # strip left side
+    pun_text = pun_text[0 : pun_text.find('&quot;')]                              # strip right side
+    pun_text = pun_text.replace('&rsquo;', '\'')                                  # Factor in apostrophe encoding
+
     embed = discord.Embed(color=0x1abc9c)
     embed.add_field(name='😒 Have A Pun', value='```\n' + pun_text + '\n```')
     await cmd.bot.send_message(message.channel, None, embed=embed)
