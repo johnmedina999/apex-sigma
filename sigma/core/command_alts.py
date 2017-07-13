4abc452aca095afd3d6e35eaa7bf3877e3d3e7ba
abraker95 <abraker95@gmail.com>
2017-07-12 20:28:18 -0400

    Add output to see what what plugin failed to load and why

--- sigma/core/command_alts.py
+++ sigma/core/command_alts.py
@@ -3,19 +3,36 @@ import yaml
 
 
 def load_alternate_command_names():
+
     alts = {}
     directory = 'sigma/plugins'
+
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file == 'plugin.yml':
-                file_path = (os.path.join(root, file))
-                with open(file_path) as plugin_file:
-                    plugin_data = yaml.safe_load(plugin_file)
-                    if plugin_data['enabled']:
-                        if 'commands' in plugin_data:
-                            for command in plugin_data['commands']:
-                                if 'alts' in command:
-                                    for alt in command['alts']:
-                                        plugin_name = command['name']
-                                        alts.update({alt: plugin_name})
+            if file != 'plugin.yml': continue
+            file_path = (os.path.join(root, file))
+
+            with open(file_path) as plugin_file:
+                plugin_data = yaml.safe_load(plugin_file)
+                
+                if not plugin_data['enabled']: continue
+                if 'commands' not in plugin_data: continue
+                
+                try:
+                    for command in plugin_data['commands']:
+                        if 'alts' not in command: continue
+                    
+                        for alt in command['alts']:
+                            plugin_name = command['name']
+                            alts.update({alt: plugin_name})
+                except TypeError:
+                    print("[ERROR] Unable to load the " + str(plugin_data['name'] + " plugin"))
+
+                    if plugin_data['commands'] == None:
+                        print("        REASON: Commands tag in plugin.py exists but no command is listed")
+
+                    if plugin_data['events'] == None:
+                        print("        REASON: Events tag in plugin.py exists but no command is listed")
+                        
+                    raise
     return alts
