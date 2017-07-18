b61c05a8c9724a5cd615b484939776fc8b499c55
abraker95 <abraker95@gmail.com>
2017-07-18 00:07:53 -0400

    Make the function respond to a role name instead

--- sigma/plugins/special/master_color.py
+++ sigma/plugins/special/master_color.py
@@ -6,22 +6,9 @@ from sigma.core.permission import check_admin
 from config import permitted_id
 
 
-
 async def master_color(ev):
-    ev.bot.loop.create_task(colorer(ev))
 
-
-
-async def colorer(ev):
-    
-    bot_owner = discord.utils.find(lambda x: x.id == permitted_id[0], ev.bot.get_all_members())
-    if not bot_owner: 
-        print("[WARNING] master_color.py: bot owner not found in any servers")
-        return
-
-    servers = ev.bot.guilds
     tick = 0.0
-
     while True:
         
         tick = tick + 1.0
@@ -32,11 +19,9 @@ async def colorer(ev):
             green_out = int((math.sin(freq*23.0*tick + 0.0) * 127.0 + 128.0))
             blue_out = int((math.sin(freq*7.0*tick + 4.0) * 127.0 + 128.0))
 
-            for server in servers:
-                if server.owner != bot_owner: continue
-                if len(server.owner.top_role.members) > 1: continue
-
-                await server.owner.top_role.edit(colour=discord.Colour.from_rgb(red_out, green_out, blue_out))
+            for guild in ev.bot.guilds:
+                master_color_role = [role for role in guild.roles if role.name == "master_color"][0]
+                await master_color_role.edit(colour=discord.Colour.from_rgb(red_out, green_out, blue_out))
         except: pass
 
         await asyncio.sleep(1)
