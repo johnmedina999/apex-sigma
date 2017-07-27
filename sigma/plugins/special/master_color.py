088fec07e8694e0f50f77ada26713dce4886849c
abraker95 <abraker95@gmail.com>
2017-07-27 02:42:44 -0400

    Fix command bailing on the first server it sees not have the master_color role

--- sigma/plugins/special/master_color.py
+++ sigma/plugins/special/master_color.py
@@ -11,14 +11,14 @@ async def master_color(ev):
         tick = tick + 1.0
         freq = (math.pi * 2.0) / 86400.0 * 10.0
 
-        try:
-            red = int((math.sin(freq*30.0*tick + 2.0) * 127.0 + 128.0))
-            green = int((math.sin(freq*23.0*tick + 0.0) * 127.0 + 128.0))
-            blue = int((math.sin(freq*7.0*tick + 4.0) * 127.0 + 128.0))
+        red = int((math.sin(freq*30.0*tick + 2.0) * 127.0 + 128.0))
+        green = int((math.sin(freq*23.0*tick + 0.0) * 127.0 + 128.0))
+        blue = int((math.sin(freq*7.0*tick + 4.0) * 127.0 + 128.0))
 
-            for guild in ev.bot.guilds:
+        for guild in ev.bot.guilds:
+            try:
                 master_color_role = [role for role in guild.roles if role.name == "master_color"][0]
                 await master_color_role.edit(colour=discord.Colour.from_rgb(red, green, blue))
-        except: pass
+            except: pass
 
         await asyncio.sleep(1)
