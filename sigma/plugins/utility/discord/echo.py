184e1467b06873315dc7ce668c00917a3dbf6b84
abraker95 <abraker95@gmail.com>
2017-10-25 23:54:20 -0400

    Move the echo command from administration to utils and make sure nobody without mention everyone can trigger an everyone highlight

---
+++ sigma/plugins/utility/discord/echo.py
@@ -0,0 +1,11 @@
+﻿from sigma.core.permission import check_mention_everyone
+
+
+async def echo(cmd, message, args):
+    msg = ' '.join(args)
+
+    print(check_mention_everyone(message.author, message.channel))
+    if not check_mention_everyone(message.author, message.channel):
+        msg = msg.replace('@everyone', 'everyone')
+
+    await message.channel.send(msg)
