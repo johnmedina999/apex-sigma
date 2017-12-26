3748653438c519c68ae0a3a9d416acd667072abd
abraker95 <abraker95@gmail.com>
2017-12-25 20:56:01 -0500

    Redo the BBcodeCompiler code due to it being convoluted and having a fatal bug which caused it to choke on computer memory

--- sigma/plugins/games/osu/BBcodeProcessor/DiscordBBcodeCompiler.py
+++ sigma/plugins/games/osu/BBcodeProcessor/DiscordBBcodeCompiler.py
@@ -3,164 +3,191 @@ import discord
 
 class DiscordBBcodeCompiler():
 
-    PARSE_END   = -1
-    PARSE_BEGIN = 0
-    PARSE_NEXT  = 1
+    BUFFER_SIZE = 2048
 
     def Compile(self, text, posts):
-        self.buffer_size = 2048
-        self.posts = posts; self.beg = 0
-
-        compile_state = self.PARSE_BEGIN
-        
-        while compile_state != self.PARSE_END:
-            self.end = min(len(text), self.beg + self.buffer_size)
-            if self.beg >= self.end: break
-
-            self.embed = discord.Embed(type='rich', color=0x66CC66)
-            self.buffer = text[self.beg:self.end]
-            
-            if self.parseImage()           == self.PARSE_BEGIN: continue
-            if self.parseImageSecure()     == self.PARSE_BEGIN: continue
-            if self.parseVideo()           == self.PARSE_BEGIN: continue
-            if self.parseText()            == self.PARSE_BEGIN: continue
-            if self.parseHyperlink()       == self.PARSE_BEGIN: continue
-            if self.parseHyperlinkSecure() == self.PARSE_BEGIN: continue
-            if self.parseNewline()         == self.PARSE_BEGIN: continue
-            if self.parseContentChar()     == self.PARSE_BEGIN: continue
-            if self.parseDefault()         == self.PARSE_BEGIN: continue
-
-        return self.posts
+        if self.parseImage(text, posts):       return
+        if self.parseSecureImage(text, posts): return
+        if self.parseVideo(text, posts):       return
+        if self.parseLink(text, posts):        return
+        if self.parseSecureLink(text, posts):  return
+        if self.parseNewline(text, posts):     return
+        if self.parseSpace(text, posts):       return
+        if self.parseText(text, posts):        return
 
 
-    # TODO: Figure out how to simplify all into a genric method
-    def parseToken(self, token_begin, token_end, token_replace):
-        # If found, then check if there is something to record before that
-        idx = self.buffer.find(token_begin, 0, len(self.buffer))
-        if idx != -1:
-            #print("img")
-            if idx == 0:
-                idx = self.buffer.find(token_end, 0, self.end)
-                self.embed.set_image(url=self.buffer[0:idx].replace(token_begin, token_end))
-            else:
-                self.embed.description = self.buffer[0:idx].replace(token_end,'').strip()
-                if self.embed.description: self.posts.append(self.embed)
+    def parseImage(self, text, posts):
+        token_len = len('img://')
+        beg = text.rfind('img://', 0, self.BUFFER_SIZE + token_len)
+        if beg == -1: return False
 
-            self.beg += idx
-            return self.PARSE_BEGIN
+        token_len = 1
+        end = min(text.find(' ', beg), text.find('\n', beg))
+        if end == -1 or end ==  0: return False
+
+        self.Compile(text[:beg], posts)
+
+        content = text[beg:end + token_len].replace('img','http').replace('\x03', '').strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseImage')
+
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.set_image(url=content)
+            posts.append(embed)
+
+        self.Compile(text[end + token_len:], posts)
+        return True
+
+
+    def parseSecureImage(self, text, posts):
+        token_len = len('imgs://')
+        beg = text.rfind('imgs://', 0, self.BUFFER_SIZE + token_len)
+        if beg == -1: return False
+
+        token_len = 1
+        end = min(text.find(' ', beg), text.find('\n', beg))
+        if end == -1 or end ==  0: return False
+
+        self.Compile(text[:beg], posts)
+
+        content = text[beg:end + token_len].replace('imgs','https').replace('\x03', '').strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseSecureImage')
+            print(content)
+
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.set_image(url=content)
+            posts.append(embed)
+
+        self.Compile(text[end + token_len:], posts)
+        return True
 
     
-    def parseImage(self):
-        # If found, then check if there is something to record before that
-        idx = self.buffer.find('img://', 0, len(self.buffer))
-        if idx != -1:
-            #print("img")
-            if idx == 0:
-                idx = self.buffer.find('\x03', 0, self.end)
-                self.embed.set_image(url=self.buffer[0:idx].replace('img','http'))
-            else:
-                self.embed.description = self.buffer[0:idx].replace('\x03','').strip()
-                if self.embed.description: self.posts.append(self.embed)
+    def parseVideo(self, text, posts):
+        token_len = len('vids://')
+        beg = text.rfind('vids://', 0, self.BUFFER_SIZE + token_len)
+        if beg == -1: return False
 
-            self.beg += idx
-            return self.PARSE_BEGIN
+        token_len = 1
+        end = min(text.find(' ', beg), text.find('\n', beg))
+        if end == -1 or end ==  0: return False
 
+        self.Compile(text[:beg], posts)
 
-    def parseImageSecure(self):
-        # If found, then check if there is something to record before that
-        idx = self.buffer.find('imgs://', 0, len(self.buffer))
-        if idx != -1: 
-            #print("imgs")
-            if idx == 0: # Record img
-                idx = self.buffer.find('\x03', 0, self.end)
-                self.embed.set_image(url=self.buffer[0:idx].replace('imgs','https'))
-                #print(buffer[0:idx])
-                self.posts.append(self.embed)
-            else:
-                self.embed.description = self.buffer[0:idx].replace('\x03','').strip()
-                if self.embed.description: self.posts.append(self.embed)
-            
-            self.beg += idx
-            return self.PARSE_BEGIN
+        content = text[beg:end + token_len].replace('vids','https').replace('\x03', '').strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseVideo')
 
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.description = content
+            posts.append(embed)
 
-    def parseVideo(self):
-        # If found, then see if we can get the link's end. If not, get everything prior
-        idx = self.buffer.rfind('vids://', 0, len(self.buffer))
-        if idx != -1:
-            #print("vids")
-            link_end = self.buffer.rfind('\x03', 0, self.end)
-            if link_end != -1: idx = link_end
+        self.Compile(text[end + token_len:], posts)
+        return True
+    
 
-            self.embed.description = self.buffer[0:idx].replace('vids','https').replace('\x03','').strip()
-            if self.embed.description: self.posts.append(self.embed)
-            self.beg += idx
-            return self.PARSE_BEGIN
+    def parseLink(self, text, posts):
+        token_len = len('http://')
+        beg = text.rfind('http://', 0, self.BUFFER_SIZE + token_len)
+        if beg == -1: return False
+
+        token_len = 1
+        end = min(text.find(' ', beg), text.find('\n', beg))
+        if end < self.BUFFER_SIZE: return False
+
+        self.Compile(text[:beg], posts)
+        
+        content = text[beg:end + token_len].replace('\x03', '').strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseLink')
+
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.description = content
+            posts.append(embed)
+
+        self.Compile(text[end + token_len:], posts)
+        return True
 
 
-    def parseText(self):
-        # If the post fits well below the lenth, just record it. 
-        # Otherwise, start looking at the best way to split it up
-        if len(self.buffer) < (self.buffer_size - 200):
-            #print("just recorded")
-            self.embed.description = self.buffer[0:len(self.buffer)].replace('\x03','').strip()
-            if self.embed.description: self.posts.append(self.embed)
-            self.beg += len(self.buffer)
-            return self.PARSE_BEGIN
+    def parseSecureLink(self, text, posts):
+        token_len = len('https://')
+        beg = text.rfind('https://', 0, self.BUFFER_SIZE + token_len)
+        if beg == -1: return False
+
+        token_len = 1
+        end = min(text.find(' ', beg), text.find('\n', beg))
+        if end < self.BUFFER_SIZE: return False
+
+        self.Compile(text[:beg], posts)
+
+        content = text[beg:end + token_len].replace('\x03', '').strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseSecureLink')
+
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.description = content
+            posts.append(embed)
+
+        self.Compile(text[end + token_len:], posts)
+        return True
 
 
-    def parseHyperlink(self):
-        # If found, then see if we can get the link's end. If not, get everything prior
-        idx = self.buffer.rfind('http://', 0, len(self.buffer))
-        if idx != -1: 
-            #print("http")
-            link_end = self.buffer.rfind('\x03', 0, self.end)
-            if link_end != -1: idx = link_end
+    def parseNewline(self, text, posts):
+        token_len = len('\n')
+        beg = 0
+        if beg == -1: return False
 
-            self.embed.description = self.buffer[0:idx].replace('\x03','').strip()
-            if self.embed.description: self.posts.append(self.embed)
-            self.beg += idx
-            return self.PARSE_BEGIN
+        end = text.rfind('\n', 0, self.BUFFER_SIZE + token_len)
+        if end < self.BUFFER_SIZE - 25: return False
+        
+        content = text[beg:end + token_len].strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseNewline')
 
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.description = content
+            posts.append(embed)
 
-    def parseHyperlinkSecure(self):
-        # If found, then see if we can get the link's end. If not, get everything prior
-        idx = self.buffer.rfind('https://', 0, len(self.buffer))
-        if idx != -1:
-            #print("https")
-            link_end = self.buffer.rfind('\x03', 0, self.end)
-            if link_end != -1: idx = link_end
+        self.Compile(text[end + token_len:], posts)
+        return True
 
-            self.embed.description = self.buffer[0:idx].replace('\x03','').strip()
-            if self.embed.description: self.posts.append(self.embed)
-            self.beg += idx
-            return self.PARSE_BEGIN
+    
+    def parseSpace(self, text, posts):
+        token_len = len(' ')
+        beg = 0
+        if beg == -1: return False
 
+        end = text.rfind(' ', 0, self.BUFFER_SIZE + token_len)        
+        if end < self.BUFFER_SIZE - 25: return False
+        
+        content = text[beg:end + token_len].strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseSpace')
 
-    def parseNewline(self):
-        # If found, then record the range
-        idx = self.buffer.rfind('\n', 0, len(self.buffer))
-        if idx != -1:
-            #print("NL")
-            self.embed.description = self.buffer[0:idx].replace('\x03','').strip()
-            if self.embed.description: self.posts.append(self.embed)
-            self.beg += idx + 1
-            return self.PARSE_BEGIN
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.description = content
+            posts.append(embed)
 
-
-    def parseContentChar(self):
-        # If found, then record the range
-        idx = self.buffer.rfind(' ', 0, len(self.buffer))
-        if idx != -1: 
-            #print("space")
-            self.embed.description = self.buffer[0:idx].replace('\x03','').strip()
-            if self.embed.description: self.posts.append(self.embed)
-            self.beg += idx + 1
-            return self.PARSE_BEGIN
+        self.Compile(text[end + token_len:], posts)
+        return True
 
 
-    def parseDefault(self):
-        self.embed.description = self.buffer.replace('\x03','').strip()
-        if self.embed.description: self.posts.append(self.embed)
-        self.beg += len(self.buffer)
-        return self.PARSE_BEGIN
+    def parseText(self, text, posts):
+        token_len = len('')
+        
+        beg = 0
+        if beg == -1: return False
+
+        end = text.rfind('', 0, self.BUFFER_SIZE + token_len)
+        if end == -1 or end ==  0: return False
+        
+        content = text[beg:end].strip()
+        if content:
+            print('DiscordBBcodeCompiler.py : parseText')
+
+            embed = discord.Embed(type='rich', color=0x66CC66)
+            embed.description = content
+            posts.append(embed)
+
+        self.Compile(text[end + token_len:], posts)
+        return True
