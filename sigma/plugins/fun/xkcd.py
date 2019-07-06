7c4fef6decc96df862ff1ce97ecceba8318a7bec
John Medina <johnmedina999@gmail.com>
2019-07-06 14:49:11 -0700

    Added automatic updating to max comic number (xkcd)

--- sigma/plugins/fun/xkcd.py
+++ sigma/plugins/fun/xkcd.py
@@ -2,24 +2,36 @@ import random
 import aiohttp
 import discord
 
-
 async def xkcd(cmd, message, args):
 
-    comic_no = str(random.randint(1, 2171))
+    random_comic = True
     if args:
         if not is_int(args[0]):
             await message.channel.send(None, embed=discord.Embed(title=':exclamation: Invalid number', color=0x993333))
             return
-        
+
         comic_no = args[0]
-        
-    joke_url = 'http://xkcd.com/' + comic_no + '/info.0.json'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(joke_url) as data:
-            try: joke_json = await data.json()
-            except Exception:
-                await message.channel.send(None, embed=discord.Embed(title=':exclamation: Requested xkcd doesn\'t exist', color=0x993333))
-                return
+        random_comic = False
+
+    while True:
+        if not random_comic:
+            joke_url = 'http://xkcd.com/' + comic_no + '/info.0.json'
+        else:
+            joke_url = 'http://xkcd.com/info.0.json'
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(joke_url) as data:
+                try:
+                    joke_json = await data.json()
+                except Exception:
+                    await message.channel.send(None, embed=discord.Embed(title=':exclamation: Requested xkcd doesn\'t exist', color=0x993333))
+                    return
+
+                if random_comic:
+                    comic_no = str(random.randint(1, joke_json['num']))
+                    random_comic = False
+                    continue
+                break
 
     embed = discord.Embed(color=0x1abc9c, title='🚽 xkcd Comic #{}: {}'.format(comic_no, joke_json['title']) ).set_image(url=joke_json['img'])
     embed.set_footer(text=joke_json['alt'])
