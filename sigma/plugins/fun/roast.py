d04ed04087a264c0fba73c4d817f922544415339
John Medina <johnmedina999@gmail.com>
2019-07-07 21:06:38 -0700

    Fixed content retrieval

--- sigma/plugins/fun/roast.py
+++ sigma/plugins/fun/roast.py
@@ -14,16 +14,19 @@ async def roast(cmd, message, args):
             page = await data.text()
 
     root = BeautifulSoup(page, 'html.parser')
-    roast_html = root.find_all(class_='oneliner')
+    content = [str(i)[3:-4] for i in root.find_all('p')[:-3]]
+        # str(i) for converting tag type into string
+        # [3:-4] for cutting out the <p></p> tags
+        # [:-3] because there are always 3 lines that are <p> tags but not roasts
 
-    if len(roast_html) != 10:
+    if len(content) != 10:
         cmd.log.error(f'Error parsing http://onelinefun.com/insults/{page_num}/')
         embed = discord.Embed(color=0xDB0000, title='Something went wrong. Notify bot owner.')
         await message.channel.send(None, embed=embed)
         return
 
     roast_choose = random.randint(0, 9)
-    roast_text = roast_html[roast_choose].find('p').text
+    roast_text = content[roast_choose]
 
     embed = discord.Embed(color=0xCC6666)
     embed.add_field(name=':fire::fire::fire::fire::fire::fire:', value=f'<@%s> {roast_text} ' % target.id)
