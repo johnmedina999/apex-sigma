a81bf52642be1200466d8d52d0cd5c993e244942
John Medina <johnmedina999@gmail.com>
2019-07-09 14:29:59 -0700

    Added custom MemberNotFoundError exception

--- sigma/core/find_member.py
+++ sigma/core/find_member.py
@@ -1,4 +1,10 @@
-async def find_member(guild, user):
+import discord
+
+class MemberNotFoundError(discord.DiscordException):
+    pass
+
+async def find_member(user, guild=discord.Guild):
     for member in guild.members:
         if user in (member.nick, member.display_name, member.id, member.name):
             return member
+    raise MemberNotFoundError(f'Member {user} not found.')
