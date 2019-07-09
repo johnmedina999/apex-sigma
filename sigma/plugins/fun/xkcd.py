b90483b94b702780adb330b942e5bea20042d7b4
John Medina <johnmedina999@gmail.com>
2019-07-08 17:58:34 -0700

    Condensed retrieval of comic

--- sigma/plugins/fun/xkcd.py
+++ sigma/plugins/fun/xkcd.py
@@ -14,35 +14,23 @@ async def xkcd(cmd, message, args):
 
     random_comic = (len(args) == 0) #True if args is empty, False otherwise
 
-    if not random_comic:
-        joke_url = 'http://xkcd.com/' + comic_no + '/info.0.json'
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(joke_url) as data:
-                try: joke_json = await data.json()
-                except Exception:
-                    await message.channel.send(None, embed=discord.Embed(title=':exclamation: Requested xkcd doesn\'t exist', color=0x993333))
-                    return
+    if not random_comic: joke_url = 'http://xkcd.com/' + comic_no + '/info.0.json'
     else:
         # To grab info of latest comic
-        joke_url = 'http://xkcd.com/info.0.json'
         async with aiohttp.ClientSession() as session:
-            async with session.get(joke_url) as data:
-                try: joke_json = await data.json()
-                except Exception:
-                    await message.channel.send(None, embed=discord.Embed(title=':exclamation: Requested xkcd doesn\'t exist', color=0x993333))
-                    return
-
-        # To grab info of comic to be displayed
+            async with session.get('http://xkcd.com/info.0.json') as data:
+                joke_json = await data.json()
         joke_url = 'http://xkcd.com/' + str(random.randint(1, joke_json['num'])) + '/info.0.json'
-        async with aiohttp.ClientSession() as session:
-            async with session.get(joke_url) as data:
-                try: joke_json = await data.json()
-                except Exception:
-                    await message.channel.send(None, embed=discord.Embed(title=':exclamation: Requested xkcd doesn\'t exist', color=0x993333))
-                    return
 
-    embed = discord.Embed(color=0x1abc9c, title='🚽 xkcd Comic #{}: {}'.format(comic_no, joke_json['title']) ).set_image(url=joke_json['img'])
+    # To grab info of comic to be displayed
+    async with aiohttp.ClientSession() as session:
+        async with session.get(joke_url) as data:
+            try: joke_json = await data.json()
+            except Exception:
+                await message.channel.send(None, embed=discord.Embed(title=':exclamation: Requested xkcd doesn\'t exist', color=0x993333))
+                return
+
+    embed = discord.Embed(color=0x1abc9c, title='🚽 xkcd Comic #{}: {}'.format(joke_json['num'], joke_json['title']) ).set_image(url=joke_json['img'])
     embed.set_footer(text=joke_json['alt'])
 
     await message.channel.send(None, embed=embed)
