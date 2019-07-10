3a8c6c1f4e569700aee3c90a86fe7988aae0a6aa
John Medina <johnmedina999@gmail.com>
2019-07-10 12:27:11 -0700

    Removed trailing whitespaces

--- sigma/plugins/searches/food/recipe.py
+++ sigma/plugins/searches/food/recipe.py
@@ -5,7 +5,7 @@ from config import Food2ForkAPIKey
 
 
 async def recipe(cmd, message, args):
-    
+
     if not args:
         await message.channel.send(cmd.help())
         return
@@ -15,28 +15,28 @@ async def recipe(cmd, message, args):
         embed.add_field(name=f':exclamation: API key Food2ForkAPIKey not found.', value='Please ask the bot owner to add it.')
         await message.channel.send(None, embed=embed)
         return
-    
+
     search = ' '.join(args)
     url = 'http://food2fork.com/api/search?key=' + Food2ForkAPIKey + '&q=' + search
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as data:
             search_data = await data.read()
             search_data = json.loads(search_data)
-    
+
     count = search_data['count']
     if count == 0:
         embed = discord.Embed(color=0x696969, title='🔍 No results were found for that, sorry.')
         await message.channel.send(None, embed=embed)
         return
-    
-    
+
+
     info = search_data['recipes'][0]
     title = info['title']
     source = info['publisher']
     source_url = info['source_url']
     image_url = info['image_url']
     publisher_url = info['publisher_url']
-        
+
     embed = discord.Embed(color=0x1abc9c)
     embed.set_author(name=source, url=publisher_url, icon_url='https://i.imgur.com/RH8LNdQ.png')
     embed.add_field(name=title, value='[**Recipe Here**](' + source_url + ')')
